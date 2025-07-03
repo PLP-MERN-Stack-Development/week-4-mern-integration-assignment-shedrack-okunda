ac01b668d9ac934c3a88cec40c19e9d57de877be
Sheddy <okundashedrack@gmail.com>
2025-07-03 11:52:19 +0300

    commented out author to be used later

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -3,7 +3,7 @@ const { body } = require("express-validator");
 exports.validatePost = [
 	body("title").notEmpty().withMessage("Title is required"),
 	body("content").notEmpty().withMessage("Content is required"),
-	body("author").notEmpty().withMessage("Author ID is required"),
+	// body("author").notEmpty().withMessage("Author ID is required"),
 	body("category").notEmpty().withMessage("Category ID is required"),
 ];
 
